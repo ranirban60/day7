c4c309223f9a561b1fdad9cf6076e65d2403af9d
ranirban60@gmail.com <ranirban60@gmail.com>
2022-06-06 12:48:51 +0530

    Added File

--- array.sh
+++ array.sh
@@ -5,13 +5,14 @@ do
 	r=$(( $RANDOM % 900 + 100 ))
 echo $r
 if [ $s1 -gt $r ]
-then 
- s2=$s1
+then
+s2=$s1
 s1=$r
 elif [ \( $s2 -gt $r \) -a \( $s1 -ne $r \) ]
 then
 s2=$r
 fi
 done
-echo 
 echo "Smallest $s1"
+echo "Second Smallest $s2"
+
